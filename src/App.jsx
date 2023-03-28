cebcc3598ac5b45628480f79a8e492a84e84ac11
Vincent Louie  Abad <vincent.abad@reddragonfarm.com>
2023-03-28 20:15:46 +0800

    with conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,40 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import Card from './components/Card'
+import data from './data'
 
-export default function App () {
+
+const App = () => {
+
+
+  const cards = data.map(item => {
+    return (
+      <Card 
+        key = {item.id}
+        img = {`./src/assets/images/${item.coverImg}`}
+        rating = {item.stats.rating}
+        reviewCount = {item.stats.reviewCount}
+        country = {item.location}
+        title = {item.title}
+        price = {item.price}
+      />
+    )
+  })
+
   return (
     <div className='container'>
       <Navbar />
-      <Banner />
-      <Card />
+      <div className='content' >
+        <Banner />
+        <section className='stories'>
+          {cards}
+        </section>
+      </div >
+
+      
+
     </div>
   )
 }
+
+export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,22 @@
 import React  from "react";
-import photo from '../assets/images/katie-zaferes.png'
 import star from '../assets/images/star.png'
 
-export default function Card () {
+const Card = (props) => {
+    const {img, rating, reviewCount, country, title, price} = props
     return (
         <div className="card">
-            <img src={photo} className='card--photo' />
+            <img src={img} className='card--photo' />
 
             <div className="card--stats">
                 <img src={star} className='card--star' />
-                <span >5.0</span>
-                <span className="gray">(6) - </span>
-                <span className="gray">USA</span>
+                <span >{rating}</span>
+                <span className="gray">({reviewCount}) - </span>
+                <span className="gray">{country}</span>
             </div>
-                <p >Life lessons with Katie Zaferes</p>
-                <p ><span className="bold">From 136</span>/ persons</p>
+                <p className="card--title">{title}</p>
+                <p className="card--price"><span className="bold">From {`$ ${price}`}</span>/ persons</p>
         </div>
     )
 }
+
+export default Card;
